a35e4c261be1c8bd894829a0d44f2ab9481bcc16
Aabhusan <maharjanaabhusan@gmail.com>
2019-11-07 13:42:47 +0545

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_domain_name" {
-  value = "aws_s3_bucket.main.bucket_domain_name"
+  value = aws_s3_bucket.main.website_endpoint
 }
 
 output "domain_name" {
-  value = "aws_s3_bucket.main1.bucket_domain_name"
+  value = aws_s3_bucket.main1.website_endpoint
 }
