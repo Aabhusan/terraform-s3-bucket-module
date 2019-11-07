8977b401b24039ac381348c4c097b6d6e0bce4a1
Aabhusan <maharjanaabhusan@gmail.com>
2019-11-07 13:11:54 +0545

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "bucket_domain_name" {
-  value = aws_s3_bucket.main.bucket_domain_name
-  description = "FQDN of bucket"
+  value = "aws_s3_bucket.main.bucket_domain_name"
+}
+
+output "domain_name" {
+  value = "aws_s3_bucket.main1.bucket_domain_name"
 }
