e1a231939ba358fcb56aca56fd8a082a750767ba
Aabhusan <maharjanaabhusan@gmail.com>
2019-11-08 12:34:45 +0545

    variables added

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "s3_bucket_name" {
+
+type = "string"
+default = "demo-static-hosting"
+description ="name of the s3 bucket"
+
+  
+}
+
+variable "s3_tags" {
+
+type = "maps"
+description="tags given in the s3 bucket"
+default= {
+    created_by= "terraform"
+    environment = "test"
+
+
+}  
+}
+
+variable "s3_regions" {
+
+type = "list"
+default = ["ap-southeast-2","us-east-2"] 
+ 
+}
+
