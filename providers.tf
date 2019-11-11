f20fb1ec5b7e4e440bef62ae2a5df816a9ce4a1c
Aabhusan <maharjanaabhusan@gmail.com>
2019-11-11 16:47:21 +0545

    remote stage

--- providers.tf
+++ providers.tf
@@ -1,3 +1,17 @@
 provider "aws" {
     region = "${var.s3_region}"
 }
+
+terraform{
+    required_version=">0.11.7"
+    backend "s3"{
+    bucket="test-aabhusan-1-us-east-1-test"
+    
+    key= "test/demo-s3"
+    
+    region="us-east-1"
+
+    encrypt="true"
+
+    }
+}
